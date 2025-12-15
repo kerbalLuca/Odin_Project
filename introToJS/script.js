433599bf0edc9000817adcc878ef5b0e67f7f02c
TheKerbal <245550589+kerbalLuca@users.noreply.github.com>
2025-12-15 18:23:12 +0100

    Understand conditional now, the semi colon is annoying

--- introToJS/script.js
+++ introToJS/script.js
@@ -1,4 +1,27 @@
+/*
 const max = 57;
 const actual = max - 13;
 const percentage = actual/max;
 console.log(percentage);
+*/
+
+//Strings
+
+let a= "Hello, I am a kerbal";
+let a_length = a.length;
+console.log(a_length);
+console.log(a.at(0));
+//Conditionals
+
+let b = 2;
+if (b == 2) {
+        console.log("b is equal to 2");
+} else {
+        console.log("Nope");
+}
+
+switch(b) {
+        case 2:
+                console.log("Switch got it");
+		break
+}
